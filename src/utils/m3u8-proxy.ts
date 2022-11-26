f59ace9bc830be32119e9ae79b5dc57e6593227e
Marouane <57333995+riimuru@users.noreply.github.com>
2022-11-25 21:50:09 -0500

    Update m3u8-proxy.ts

--- src/utils/m3u8-proxy.ts
+++ src/utils/m3u8-proxy.ts
@@ -9,28 +9,33 @@ class M3U8Proxy {
   };
 
   public getM3U8Proxy = async (fastify: FastifyInstance, options: RegisterOptions) => {
-    fastify.get('/m3u8-proxy', async (request: FastifyRequest, reply: FastifyReply) => {
-      const { url } = request.query as { url: string };
-      // get headers from the query
-      const { referer } = request.query as { referer: string };
+    fastify.get(
+      '/m3u8-proxy.m3u8',
+      async (request: FastifyRequest, reply: FastifyReply) => {
+        const { url } = request.query as { url: string };
+        // get headers from the query
+        const { referer } = request.query as { referer: string };
 
-      if (!url) {
-        reply.status(400).send('No URL provided');
-        return;
+        if (!url) {
+          reply.status(400).send('No URL provided');
+          return;
+        }
+
+        reply.header('Content-Type', 'application/x-mpegURL');
+        reply.header('Access-Control-Allow-Origin', '*');
+
+        // return the image
+        reply.send(
+          await this.getM3U8(decodeURIComponent(url), {
+            headers: {
+              referer: referer,
+              'User-Agent':
+                'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36 Edg/107.0.1418.35',
+            },
+          })
+        );
       }
-
-      // return the image
-      reply.send(
-        await this.getM3U8(decodeURIComponent(url), {
-          headers: {
-            referer: referer,
-            'User-Agent':
-              'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36 Edg/107.0.1418.35',
-          },
-          responseType: 'arraybuffer',
-        })
-      );
-    });
+    );
   };
 }
 
