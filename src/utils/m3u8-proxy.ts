5d394638c1f0ac6a02d6d9a8740a719fb92eea23
Marouane <57333995+riimuru@users.noreply.github.com>
2022-11-25 22:06:11 -0500

    Update m3u8-proxy.ts

--- src/utils/m3u8-proxy.ts
+++ src/utils/m3u8-proxy.ts
@@ -2,7 +2,7 @@ import axios, { AxiosRequestConfig } from 'axios';
 import { FastifyRequest, FastifyReply, FastifyInstance, RegisterOptions } from 'fastify';
 
 class M3U8Proxy {
-  private streamsbDomain: string = ""
+  private streamsbDomain: string = '';
   private getM3U8 = async (url: string, options: AxiosRequestConfig): Promise<string> => {
     const data = await axios.get(url, options);
 
@@ -13,7 +13,7 @@ class M3U8Proxy {
     fastify.get(
       '/m3u8-proxy/:url',
       async (request: FastifyRequest, reply: FastifyReply) => {
-        const { url } = request.params as {url: string}
+        const { url } = request.params as { url: string };
 
         if (!url) {
           reply.status(400).send('No URL provided');
@@ -22,14 +22,13 @@ class M3U8Proxy {
 
         reply.header('Content-Type', 'application/x-mpegURL');
         reply.header('Access-Control-Allow-Origin', '*');
-        
-        if (!streamsbDomain) reply.send('domain is null')
+
+        if (!this.streamsbDomain) reply.send('domain is null');
 
         // return the image
         reply.send(
           await this.getM3U8(this.streamsbDomain + decodeURIComponent(url), {
             headers: {
-              referer: referer,
               'User-Agent':
                 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36 Edg/107.0.1418.35',
             },
@@ -37,12 +36,12 @@ class M3U8Proxy {
         );
       }
     );
-    
+
     fastify.get(
       '/m3u8-proxy/m3u8',
       async (request: FastifyRequest, reply: FastifyReply) => {
         const { url } = request.query as { url: string };
-        const { domain } = request.query as { domain: string }
+        const { domain } = request.query as { domain: string };
         // get headers from the query
         const { referer } = request.query as { referer: string };
 
@@ -50,8 +49,8 @@ class M3U8Proxy {
           reply.status(400).send('No URL provided');
           return;
         }
-        
-        this.streamsbDomain = domain
+
+        this.streamsbDomain = domain;
 
         reply.header('Content-Type', 'application/x-mpegURL');
         reply.header('Access-Control-Allow-Origin', '*');
