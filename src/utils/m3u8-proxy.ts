83d1f2b3ac122351ddf0bfdf477ef5ea5f9c977b
riimuru <mythhb@yahoo.com>
2022-11-25 21:30:54 -0500

    add m3u8 proxy

--- src/utils/m3u8-proxy.ts
+++ src/utils/m3u8-proxy.ts
@@ -3,9 +3,7 @@ import { FastifyRequest, FastifyReply, FastifyInstance, RegisterOptions } from '
 
 class M3U8Proxy {
   private getM3U8 = async (url: string, options: {}): Promise<string> => {
-    const data = await axios.get(url, {
-      ...options,
-    });
+    const data = await axios.get(url, options);
 
     return data.data;
   };
@@ -25,7 +23,7 @@ class M3U8Proxy {
       reply.send(
         await this.getM3U8(url, {
           headers: {
-            referer,
+            referer: referer,
             'User-Agent':
               'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36 Edg/107.0.1418.35',
           },
