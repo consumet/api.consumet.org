22986d044c65b835035b260ab4763a379e322ab6
Marouane <57333995+riimuru@users.noreply.github.com>
2022-11-25 23:43:58 -0500

    Update m3u8-proxy.ts

--- src/utils/m3u8-proxy.ts
+++ src/utils/m3u8-proxy.ts
@@ -1,18 +1,24 @@
 import axios, { AxiosRequestConfig } from 'axios';
 import { FastifyRequest, FastifyReply, FastifyInstance, RegisterOptions } from 'fastify';
+import fetch, { RequestInit } from 'node-fetch';
+import { decode } from 'punycode';
 
 class M3U8Proxy {
   private streamsbDomain: string = '';
-  private getM3U8 = async (url: string, options: AxiosRequestConfig): Promise<string> => {
-    const data = await axios.get(url, options);
+  private getM3U8 = async (
+    url: string,
+    options: RequestInit | undefined
+  ): Promise<string> => {
+    const data = await fetch(url, options);
+    const text = await data.text();
 
-    return data.data;
+    return text;
   };
 
-  private toQueryString = (obj: any): string => {
+  private toQueryString = (obj: any) => {
     const parts = [];
     for (const i in obj) {
-      if (obj.hasOwnProperty(i)) {
+      if (obj[i]) {
         parts.push(encodeURIComponent(i) + '=' + encodeURIComponent(obj[i]));
       }
     }
@@ -20,69 +26,37 @@ class M3U8Proxy {
   };
 
   public getM3U8Proxy = async (fastify: FastifyInstance, options: RegisterOptions) => {
-    fastify.get(
-      '/m3u8-proxy/:url',
-      async (request: FastifyRequest, reply: FastifyReply) => {
-        const { url } = request.params as { url: string };
+    fastify.get('/m3u8-proxy/*', async (request: FastifyRequest, reply: FastifyReply) => {
+      // split params
+      const params = (request.params as any)['*'].split('/');
+      const queries = request.query as any;
+      console.log('params', params);
 
-        if (!url) {
-          reply.status(400).send('No URL provided');
-          return;
+      // last element is the url
+      const url = params.pop();
+      // decode safe base64
+      const decodedUrl = Buffer.from(url, 'base64').toString('ascii');
+
+      const domain = Buffer.from(params.join(''), 'base64').toString('ascii');
+
+      // queries to object
+      const data = await this.getM3U8(
+        decodedUrl.startsWith('https')
+          ? decodedUrl
+          : domain + url + '?' + this.toQueryString(queries),
+        {
+          headers: {
+            'User-Agent':
+              'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36 Edg/107.0.1418.35',
+            watchsb: 'streamsb',
+          },
         }
-
-        reply.header('Content-Type', 'application/x-mpegURL');
-        reply.header('Access-Control-Allow-Origin', '*');
-
-        if (!this.streamsbDomain) reply.send('domain is null');
-
-        // return the image
-        reply.send(
-          await this.getM3U8(
-            this.streamsbDomain +
-              decodeURIComponent(url) +
-              '?' +
-              this.toQueryString(request.params),
-            {
-              headers: {
-                'User-Agent':
-                  'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36 Edg/107.0.1418.35',
-              },
-            }
-          )
-        );
-      }
-    );
-
-    fastify.get(
-      '/m3u8-proxy/m3u8',
-      async (request: FastifyRequest, reply: FastifyReply) => {
-        const { url } = request.query as { url: string };
-        const { domain } = request.query as { domain: string };
-        // get headers from the query
-        const { referer } = request.query as { referer: string };
-
-        if (!url) {
-          reply.status(400).send('No URL provided');
-          return;
-        }
-
-        this.streamsbDomain = domain;
-
-        reply.header('Content-Type', 'application/x-mpegURL');
-        reply.header('Access-Control-Allow-Origin', '*');
-
-        // return the image
-        reply.send(
-          await this.getM3U8(decodeURIComponent(url), {
-            headers: {
-              referer: referer,
-              'User-Agent':
-                'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36 Edg/107.0.1418.35',
-            },
-          })
-        );
-      }
-    );
+      );
+      //const decodedData = Buffer.from(data, 'binary').toString('utf8');
+      reply.header('Content-Type', 'application/x-mpegURL');
+      reply.header('Access-Control-Allow-Origin', '*');
+      reply.send(data);
+    });
   };
 }
 
