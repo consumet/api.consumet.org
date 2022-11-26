b699424cd5e082f12ef8abb50f9f0cc9f280b690
Marouane <57333995+riimuru@users.noreply.github.com>
2022-11-25 22:12:55 -0500

    Update m3u8-proxy.ts

--- src/utils/m3u8-proxy.ts
+++ src/utils/m3u8-proxy.ts
@@ -9,6 +9,16 @@ class M3U8Proxy {
     return data.data;
   };
 
+  private toQueryString = (obj: any): string => {
+    const parts = [];
+    for (const i in obj) {
+      if (obj.hasOwnProperty(i)) {
+        parts.push(encodeURIComponent(i) + '=' + encodeURIComponent(obj[i]));
+      }
+    }
+    return parts.join('&');
+  };
+
   public getM3U8Proxy = async (fastify: FastifyInstance, options: RegisterOptions) => {
     fastify.get(
       '/m3u8-proxy/:url',
@@ -27,12 +37,18 @@ class M3U8Proxy {
 
         // return the image
         reply.send(
-          await this.getM3U8(this.streamsbDomain + decodeURIComponent(url), {
-            headers: {
-              'User-Agent':
-                'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36 Edg/107.0.1418.35',
-            },
-          })
+          await this.getM3U8(
+            this.streamsbDomain +
+              decodeURIComponent(url) +
+              '?' +
+              this.toQueryString(request.query),
+            {
+              headers: {
+                'User-Agent':
+                  'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36 Edg/107.0.1418.35',
+              },
+            }
+          )
         );
       }
     );
