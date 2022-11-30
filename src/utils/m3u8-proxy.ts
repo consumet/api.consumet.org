681d1190ce848985374ffa9ae2e0637934bad809
Marouane <57333995+riimuru@users.noreply.github.com>
2022-11-30 14:17:15 -0500

    fix m3u8 proxy

--- src/utils/m3u8-proxy.ts
+++ src/utils/m3u8-proxy.ts
@@ -49,7 +49,7 @@ class M3U8Proxy {
           headers: {
             'User-Agent':
               'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36 Edg/107.0.1418.35',
-            watchsb: 'streamsb',
+            watchsb: 'sbstream',
           },
         }
       );
