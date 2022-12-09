9202092d8f948868b724e9f58c9804c754f3412a
InumakiEU <kempcompany05@gmail.com>
2022-12-09 21:43:46 +0100

    mal

--- src/routes/meta/index.ts
+++ src/routes/meta/index.ts
@@ -3,10 +3,12 @@ import { PROVIDERS_LIST } from '@consumet/extensions';
 
 import anilist from './anilist';
 import anilistManga from './anilist-manga';
+import mal from './mal';
 
 const routes = async (fastify: FastifyInstance, options: RegisterOptions) => {
   await fastify.register(anilist, { prefix: '/' });
   await fastify.register(anilistManga, { prefix: '/' });
+  await fastify.register(mal, { prefix: '/' });
 
   fastify.get('/', async (request: any, reply: any) => {
     reply.status(200).send('Welcome to Consumet Meta');
