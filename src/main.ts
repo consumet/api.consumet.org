419ee323a48bea33322a4d68bd822c1db6296f36
Tdanks2000 <tommydanks2000@hotmail.com>
2023-02-14 12:05:27 +0000

    added ann and updated consumet to latest version

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,7 @@ import comics from './routes/comics';
 import lightnovels from './routes/light-novels';
 import movies from './routes/movies';
 import meta from './routes/meta';
+import news from './routes/news';
 import chalk from 'chalk';
 import Utils from './utils';
 
@@ -50,6 +51,7 @@ export const tmdbApi = process.env.apiKey && process.env.apiKey;
   await fastify.register(lightnovels, { prefix: '/light-novels' });
   await fastify.register(movies, { prefix: '/movies' });
   await fastify.register(meta, { prefix: '/meta' });
+  await fastify.register(news, { prefix: '/news' });
 
   await fastify.register(Utils, { prefix: '/utils' });
 
