57c52d4876b896a35d3228aed92ed466b785303d
Tdanks2000 <40835340+TDanks2000@users.noreply.github.com>
2023-03-13 07:05:30 +0000

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -19,8 +19,6 @@ export const redis =
     host: process.env.REDIS_HOST,
     port: Number(process.env.REDIS_PORT),
     username: process.env.REDIS_USERNAME,
-    password: process.env.REDIS_PASSWORD,
-    tls: {},
   });
 
 export const tmdbApi = process.env.apiKey && process.env.apiKey;
