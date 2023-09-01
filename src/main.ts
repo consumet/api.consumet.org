3b660e0be075517a6c64417feabf6cb1bec48686
Victor Gonzales <vgonzales5157@gmail.com>
2023-09-01 10:25:31 +0800

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -28,7 +28,7 @@ export const tmdbApi = process.env.TMDB_KEY && process.env.TMDB_KEY;
   console.log(chalk.green(`Starting server on port ${PORT}... 🚀`));
   if (!process.env.REDIS_HOST)
     console.warn(chalk.yellowBright('Redis not found. Cache disabled.'));
-  if (!process.env.tmdbApi)
+  if (!process.env.TMDB_KEY)
     console.warn(
       chalk.yellowBright('TMDB api key not found. the TMDB meta route may not work.')
     );
