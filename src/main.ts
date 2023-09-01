52231ebf0749bf27ee020655c20ae75e024818ee
KuroXI <vgonzales5157@gmail.com>
2023-09-01 10:23:37 +0800

    Fix REDIS_NO_AUTH and TMDB key problem

--- src/main.ts
+++ src/main.ts
@@ -18,10 +18,10 @@ export const redis =
   new Redis({
     host: process.env.REDIS_HOST,
     port: Number(process.env.REDIS_PORT),
-    username: process.env.REDIS_USERNAME,
+    password: process.env.REDIS_PASSWORD
   });
 
-export const tmdbApi = process.env.apiKey && process.env.apiKey;
+export const tmdbApi = process.env.TMDB_KEY && process.env.TMDB_KEY;
 (async () => {
   const PORT = Number(process.env.PORT) || 3000;
 
