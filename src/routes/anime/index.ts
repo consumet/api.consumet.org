3b65546be638fe49268caa1a54a0928209691233
Carlos Esteven <carlos.cancimance00@usc.edu.co>
2024-12-14 10:40:24 -0500

    Anix

--- src/routes/anime/index.ts
+++ src/routes/anime/index.ts
@@ -10,6 +10,7 @@ import anify from './anify';
 import crunchyroll from './crunchyroll';
 import bilibili from './bilibili';
 import marin from './marin';
+import anix from './anix';
 
 const routes = async (fastify: FastifyInstance, options: RegisterOptions) => {
   await fastify.register(gogoanime, { prefix: '/gogoanime' });
@@ -21,6 +22,7 @@ const routes = async (fastify: FastifyInstance, options: RegisterOptions) => {
   await fastify.register(crunchyroll, { prefix: '/crunchyroll' });
   await fastify.register(bilibili, { prefix: '/bilibili' });
   await fastify.register(marin, { prefix: '/marin' });
+  await fastify.register(anix, { prefix: '/anix' });
 
   fastify.get('/', async (request: any, reply: any) => {
     reply.status(200).send('Welcome to Consumet Anime 🗾');
