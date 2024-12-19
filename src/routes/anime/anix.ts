0bf8d7f6fdc6aaca534687352b8536727868c12e
Carlos Esteven <carlos.cancimance00@usc.edu.co>
2024-12-19 02:39:44 -0500

    Update API Anix

--- src/routes/anime/anix.ts
+++ src/routes/anime/anix.ts
@@ -1,5 +1,6 @@
 import { FastifyRequest, FastifyReply, FastifyInstance, RegisterOptions } from 'fastify';
 import { ANIME } from '@consumet/extensions';
+import { StreamingServers } from '@consumet/extensions/dist/models';
 
 const routes = async (fastify: FastifyInstance, options: RegisterOptions) => {
   const anix = new ANIME.Anix();
@@ -53,6 +54,8 @@ const routes = async (fastify: FastifyInstance, options: RegisterOptions) => {
   fastify.get('/watch', async (request: FastifyRequest, reply: FastifyReply) => {
     const id = (request.query as { id: string }).id;
     const episodeId = (request.query as { episodeId: string }).episodeId;
+    const type = (request.query as { type: string }).type ?? 'sub';
+    const server = (request.query as { server: string }).server as StreamingServers;
 
     if (typeof id === 'undefined')
       return reply.status(400).send({ message: 'id is required' });
@@ -62,7 +65,7 @@ const routes = async (fastify: FastifyInstance, options: RegisterOptions) => {
 
     try {
       const res = await anix
-        .fetchEpisodeSources(id, episodeId)
+        .fetchEpisodeSources(id, episodeId, server, type)
         .catch((err) => reply.status(404).send({ message: err }));
 
       reply.status(200).send(res);
@@ -86,6 +89,30 @@ const routes = async (fastify: FastifyInstance, options: RegisterOptions) => {
     try {
       const res = await anix
         .fetchEpisodeServers(id, episodeId)
+        .catch((err) => reply.status(404).send({ message: err }));
+
+      reply.status(200).send(res);
+    } catch (err) {
+      reply
+        .status(500)
+        .send({ message: 'Something went wrong. Contact developer for help.' });
+    }
+  });
+
+  fastify.get('/servers-type', async (request: FastifyRequest, reply: FastifyReply) => {
+    const id = (request.query as { id: string }).id;
+    const episodeId = (request.query as { episodeId: string }).episodeId;
+    const type = (request.query as { type: string }).type ?? 'sub';
+
+    if (typeof id === 'undefined')
+      return reply.status(400).send({ message: 'id is required' });
+
+    if (typeof episodeId === 'undefined')
+      return reply.status(400).send({ message: 'episodeId is required' });
+
+    try {
+      const res = await anix
+        .fetchEpisodeServerType(id, episodeId, type)
         .catch((err) => reply.status(404).send({ message: err }));
 
       reply.status(200).send(res);
