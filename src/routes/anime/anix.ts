06043e42495651d0454475ce802ac65706e8186d
Shikiiii <imasimpforegirls@gmail.com>
2025-01-15 22:56:54 +0200

    fix(anix): Merged servers and servers-type into 1 endpoint servers

--- src/routes/anime/anix.ts
+++ src/routes/anime/anix.ts
@@ -9,7 +9,7 @@ const routes = async (fastify: FastifyInstance, options: RegisterOptions) => {
     rp.status(200).send({
       intro:
         "Welcome to the Anix provider: check out the provider's website @ https://anix.sh",
-      routes: ['/:query', '/recent-episodes', '/info/:id', '/watch/:id/:episodeId', '/servers/:id/:episodeId', '/servers-type/:id/:episodeId'],
+      routes: ['/:query', '/recent-episodes', '/info/:id', '/watch/:id/:episodeId', '/servers/:id/:episodeId'],
       documentation: 'https://docs.consumet.org/#tag/anix',
     });
   });
@@ -95,6 +95,7 @@ const routes = async (fastify: FastifyInstance, options: RegisterOptions) => {
     '/servers/:id/:episodeId',
     async (request: FastifyRequest, reply: FastifyReply) => {
       const { id, episodeId } = request.params as { id: string; episodeId: string };
+      const type = (request.query as { type?: string }).type;
   
       if (typeof id === 'undefined')
         return reply.status(400).send({ message: 'id is required' });
@@ -103,9 +104,16 @@ const routes = async (fastify: FastifyInstance, options: RegisterOptions) => {
         return reply.status(400).send({ message: 'episodeId is required' });
 
       try {
-        const res = await anix
-          .fetchEpisodeServers(id, episodeId)
-          .catch((err) => reply.status(404).send({ message: err }));
+        let res;
+        if (typeof type === 'undefined') {
+          res = await anix
+            .fetchEpisodeServers(id, episodeId)
+            .catch((err) => reply.status(404).send({ message: err }));;
+        } else {
+          res = await anix
+            .fetchEpisodeServerType(id, episodeId, type)
+            .catch((err) => reply.status(404).send({ message: err }));;
+        }
 
         reply.status(200).send(res);
       } catch (err) {
@@ -116,31 +124,6 @@ const routes = async (fastify: FastifyInstance, options: RegisterOptions) => {
       }
     },
   );
-
-  fastify.get(
-    '/servers-type/:id/:episodeId',
-    async (request: FastifyRequest, reply: FastifyReply) => {
-      const { id, episodeId } = request.params as { id: string; episodeId: string };
-      const type = (request.query as { type: string }).type ?? 'sub';
-
-      if (typeof id === 'undefined')
-        return reply.status(400).send({ message: 'id is required' });
-
-      if (typeof episodeId === 'undefined')
-        return reply.status(400).send({ message: 'episodeId is required' });
-
-      try {
-        const res = await anix
-          .fetchEpisodeServerType(id, episodeId, type)
-          .catch((err) => reply.status(404).send({ message: err }));
-
-        reply.status(200).send(res);
-      } catch (err) {
-        reply
-          .status(500)
-          .send({ message: 'Something went wrong. Contact developer for help.' });
-      }
-  });
 };
 
 export default routes;
