2d43c0ab47cebe4d0f4bd687a6965568f1e4c345
eatmynerds <eatmynerds@gmail.com>
2025-11-24 23:37:23 -0600

    refactor: update animepahe

--- src/routes/anime/animepahe.ts
+++ src/routes/anime/animepahe.ts
@@ -11,7 +11,7 @@ const routes = async (fastify: FastifyInstance, options: RegisterOptions) => {
   fastify.get('/', (_, rp) => {
     rp.status(200).send({
       intro: `Welcome to the animepahe provider: check out the provider's website @ ${animepahe.toString.baseUrl}`,
-      routes: ['/:query', '/info/:id', '/watch/:episodeId'],
+      routes: ['/:query', '/info/:id', '/watch/:episodeId', '/recent-episodes'],
       documentation: 'https://docs.consumet.org/#tag/animepahe',
     });
   });
@@ -36,6 +36,29 @@ const routes = async (fastify: FastifyInstance, options: RegisterOptions) => {
       });
     }
   });
+
+  fastify.get(
+    '/recent-episodes',
+    async (request: FastifyRequest, reply: FastifyReply) => {
+      const page = (request.query as { page: number }).page;
+      try {
+        let res = redis
+          ? await cache.fetch(
+              redis as Redis,
+              `animepahe:recent-episodes:${page}`,
+              async () => await animepahe.fetchRecentEpisodes(page),
+              REDIS_TTL,
+            )
+          : await animepahe.fetchRecentEpisodes(page);
+
+        reply.status(200).send(res);
+      } catch (error) {
+        reply.status(500).send({
+          message: 'Something went wrong. Contact developer for help.',
+        });
+      }
+    },
+  );
 
   fastify.get('/info/:id', async (request: FastifyRequest, reply: FastifyReply) => {
     const id = decodeURIComponent((request.params as { id: string }).id);
